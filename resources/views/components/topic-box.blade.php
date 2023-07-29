bab131b932552c9f8b2b53f4b809ef425343ddef
DonutEmperor <56782553+DonutEmperor@users.noreply.github.com>
2023-07-30 00:48:05 +0800

    Topic-box components

---
+++ resources/views/components/topic-box.blade.php
@@ -0,0 +1,21 @@
+<div class="card">
+    <div class="card-header">
+        <h3>Topics</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group" style="max-height: 200px; overflow-y: auto;">
+            <!-- Existing topics with links -->
+            <li class="list-group-item"><a href="{{ route('topic') }}">Topic 1</a></li>
+            <li class="list-group-item"><a href="#">Topic 2</a></li>
+            <li class="list-group-item"><a href="#">Topic 3</a></li>
+            <li class="list-group-item"><a href="#">Topic 4</a></li>
+            <li class="list-group-item"><a href="#">Topic 2</a></li>
+            <li class="list-group-item"><a href="#">Topic 3</a></li>
+            <li class="list-group-item"><a href="#">Topic 4</a></li>
+            <li class="list-group-item"><a href="#">Topic 2</a></li>
+            <li class="list-group-item"><a href="#">Topic 3</a></li>
+            <li class="list-group-item"><a href="#">Topic 4</a></li>
+            <!-- Additional topics will be added here as they grow -->
+        </ul>
+    </div>
+</div>
