f849fc730f60914dfe89f2165cd5d5de2dd247c9
DonutEmperor <56782553+DonutEmperor@users.noreply.github.com>
2023-07-30 01:06:38 +0800

    Added the addthread and addtopic blade

--- resources/views/threadpage.blade.php
+++ resources/views/threadpage.blade.php
@@ -8,26 +8,33 @@
 <div class="thread">
     <!-- This is where your content goes -->
     <div class="threadcontent">
+        <div class="row mb-4">
+            <div class="col-md-9">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="media">
+                            <h3 class="mt-0"><a href="{{ route('thread') }}">#Thread</a></h3>
+                            <h6>#Thread-ID</h6>
+                            <p>Content of Thread 1... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel erat et odio bibendum euismod.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel erat et odio bibendum euismod.
 
-        <div class="card">
-            <div class="card-body">
-                <div class="media">
-                    <h3 class="mt-0"><a href="{{ route('thread') }}">#Thread</a></h3>
-                    <h6>#Thread-ID</h6>
-                    <p>Content of Thread 1... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel erat et odio bibendum euismod.
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel erat et odio bibendum euismod.
-
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel erat et odio bibendum euismod.
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel erat et odio bibendum euismod.
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel erat et odio bibendum euismod.</p>
-                    <p class="mb-3">25 upvotes | 5 downvotes | #TimeStamp</p>
-                    <button class="btn btn-sm btn-success">^</button>
-                    <button class="btn btn-sm btn-danger">v</button>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel erat et odio bibendum euismod.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel erat et odio bibendum euismod.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel erat et odio bibendum euismod.</p>
+                            <p class="mb-3">25 upvotes | 5 downvotes | #TimeStamp</p>
+                            <button class="btn btn-sm btn-success">^</button>
+                            <button class="btn btn-sm btn-danger">v</button>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
 
+            <div class="col-md-3 text-md-right">
+                <x-topic-box />
+            </div>
+        </div>
         <br>
+
         <div>
             <h6 class="text-decoration-underline">Comments (10)</h6>
         </div>
